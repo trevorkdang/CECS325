fa74e9f8589d81b627ff54fbefa575591cce92de
TKD10 <103336180+TKD10@users.noreply.github.com>
2023-03-07 02:16:46 -0800

    finished Prog3

--- Prog3/generate.cpp
+++ Prog3/generate.cpp
@@ -7,19 +7,21 @@ int main(int argc, char* argv[])
 {
     if (argc < 4)
     {
-        cerr << "Usage: " << argv[0] << "Count Min Max" << endl;
+        cerr << "Usage: " << argv[0] << " Count Min Max" << endl;
+        return 1;
     }
-
-    for (int i = 0; i < argc; i++);
+    
+    for (int i = 0; i < argc; i++)
     {
         cout << argv[i] << endl;
     }
-
+    //accepts 3 arguments
     int count = stoi(argv[1]);
     int min = stoi(argv[2]);
     int max = stoi(argv[3]);
 
-    ofstream ofile("numbers.dat");
+    //generates an output file that holds all the pseudo random numbers
+    ofstream ofile("numbers.dat"); 
     for (int i = 0; i < count; i++)
     {
         ofile << rand() % (max - min + 1) + min << endl;
--- Prog3/mysort.cpp
+++ Prog3/mysort.cpp
@@ -1,16 +1,16 @@
 //	Trevor Dang
 //	CECS 325-02
 //	Prog 3 - Sorting Contest
-//	02/21/2023
+//	03/07/2023
 //
 //	I certify that this program is my own original work. I did not copy any part of this program 
 //	from any other source. I further certify that I typed each and every line of code in this program.
 
 #include <iostream>
 #include <fstream>
-#include <string>
 using namespace std;
 
+//bubble sort function without using swap and used a temp var instead
 void bubbleSort(int arr[], int n)
 {
     for (int i = 0; i < n-1; i++)
@@ -25,55 +25,54 @@ void bubbleSort(int arr[], int n)
             }
         }
     }
-    return 0;
 }
 
-
-const int max_nums = 1000000;
 
 int main(int argc, char* argv[])
 {
     if (argc < 3)
     {
         cerr << "Usage: " << argv[0] << "Input Output" << endl;
+        return 1;
     }
 
-    ifstream ifile(argv[1]);
+    ifstream ifile(argv[1]); //input file
     if (!ifile.is_open())
     {
-        cerr << "Failed to open input file" << argv[1] << endl;
+        cerr << "Cannot open input file" << argv[1] << endl;
     }
 
-    int numbers[max_nums];
-    int count  = 0;
+    int numbers[1000000]; //array in file that can hold 1 million numbers
+    int count = 0; //counter to keep track of numbers input file
     int nums;
-    while (ifile >> num)
+
+    while (ifile >> nums && count < 1000000) //while loop that makes sure number of inputs not more than 1 million
     {
         numbers[count] = nums;
         count++;
-        if (count >= max_nums)
+        if (count > 1000000)
         {
             cerr << "Input file contains too many numbers" << endl;
+            return 1;
         }
     }
 
-    ifile.close()
+    ifile.close();
 
-    bubbleSort(numbers, count);
+    bubbleSort(numbers, count); //runs bubble sort
 
-    ofstream ofile(argv[2]);
+    ofstream ofile(argv[2]); //output file that holds sorted array
     if (!ofile.is_open())
     {
-        cerr << "Failed to open output file" << argv[2] << endl;
+        cerr << "Cannot open output file" << argv[2] << endl;
     }
 
     for (int i = 0; i < count; i++)
     {
         ofile << numbers[i] << endl;
-        cout << numbers[i] << endl;
     }
 
-    ofile.close()
+    ofile.close();
     return 0;
 
 }

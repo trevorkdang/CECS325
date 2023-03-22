1837fe080acc85018f994d0e4f214e0615ade435
TKD10 <103336180+TKD10@users.noreply.github.com>
2023-03-21 18:35:13 -0700

    still need to finish prog4

--- Prog4/mysort.cpp
+++ Prog4/mysort.cpp
@@ -11,18 +11,24 @@
 #include <pthread.h>
 using namespace std;
 
-struct pthread_args {
-    int *arr; //pointer to array
-    int left; //start index of array
-    int right; //end index of array to sort
+struct list {
+    int *arr; 
+    int arrSize; 
+    int wait; 
 };
 
 //bubble sort function without using swap and used a temp var instead
-void bubbleSort(int arr[], int n)
+void *bubbleSort(void *arr)
 {
-    for (int i = 0; i < n-1; i++)
+    list *arg = (list *) arr
+    int *arr = arg->arr;
+    int arrSize = arg->arrSize;
+    int wait = arg->wait;
+
+    
+    for (int i = 0; i < arrSize-1; i++)
     {
-        for (int j = 0; j < n-i-1; j++)
+        for (int j = 0; j < arrSizze-i-1; j++)
         {
             if (arr[j] > arr[j+1])
             {
@@ -85,13 +91,13 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    pthread_args argList[4];
-    for (int i = 0; i < 4; i++)
+    pthread_args argList[8];
+    /*for (int i = 0; i < 4; i++)
     {
         argList[i].arr = arr[i];
         argList[i].left = l
         argList[i].right = r
-    }
+    }*/
 
     ifstream ifile(argv[1]); //input file
     if (!ifile.is_open())
@@ -102,6 +108,7 @@ int main(int argc, char* argv[])
     int numbers[1000000]; //array in file that can hold 1 million numbers
     int count = 0; //counter to keep track of numbers input file
     int nums;
+    
 
     while (ifile >> nums && count < 1000000) //while loop that makes sure number of inputs not more than 1 million
     {
@@ -116,7 +123,51 @@ int main(int argc, char* argv[])
 
     ifile.close();
 
-    bubbleSort(numbers, count); //runs bubble sort
+    int sizeArr = nums;
+    int size = nums/8;
+
+    for (int i = 0; i <8; i++)
+    {
+        argList[i].arrSize = size;
+        if (i == 0)
+        {
+            argList[i].arr = numbers;
+        }
+        else
+        {
+            argList[i].arr = argList[i-1].arr + argList[i].arrSize;
+        }
+        argList[i].wait = rand()%8;
+    }
+
+
+    pthread_t thread0, thread1, thread2, thread3, thread4, thread5, thread6, thread7;
+    int iret0, iret1, iret2, iret3, iret4, iret5, iret6, iret7;
+    
+    iret0 = pthread_create(&thread0, NULL, bubbleSort, (void*) &argList[0]);
+    iret1 = pthread_create(&thread1, NULL, bubbleSort, (void*) &argList[1]);
+    iret2 = pthread_create(&thread2, NULL, bubbleSort, (void*) &argList[2]);
+    iret3 = pthread_create(&thread3, NULL, bubbleSort, (void*) &argList[3]);
+    iret4 = pthread_create(&thread4, NULL, bubbleSort, (void*) &argList[4]);
+    iret5 = pthread_create(&thread5, NULL, bubbleSort, (void*) &argList[5]);
+    iret6 = pthread_create(&thread6, NULL, bubbleSort, (void*) &argList[6]);
+    iret7 = pthread_create(&thread7, NULL, bubbleSort, (void*) &argList[7]);
+
+    pthread_join(thread0, NULL);
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+    pthread_join(thread3, NULL);
+    pthread_join(thread4, NULL);
+    pthread_join(thread5, NULL);
+    pthread_join(thread6, NULL);
+    pthread_join(thread7, NULL);
+
+    int subArrSize = argList[1].arrSize;
+
+    for (int i = 0; )
+
+
+    
 
     ofstream ofile(argv[2]); //output file that holds sorted array
     if (!ofile.is_open())

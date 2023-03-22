1a727541ad3540893e66a3b1798f0514694ea4e1
TKD10 <103336180+TKD10@users.noreply.github.com>
2023-03-21 21:18:13 -0700

    finished prog4

--- Prog4/mysort.cpp
+++ Prog4/mysort.cpp
@@ -1,7 +1,7 @@
 //	Trevor Dang
 //	CECS 325-02
 //	Prog 4 - Sorting Contest (pthread)
-//	03/07/2023
+//	03/21/2023
 //
 //	I certify that this program is my own original work. I did not copy any part of this program 
 //	from any other source. I further certify that I typed each and every line of code in this program.

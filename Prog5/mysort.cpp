bec808f677057f4ea2515b0ee8e1312aa3d8393b
TKD10 <103336180+TKD10@users.noreply.github.com>
2023-04-05 22:14:34 -0700

    finished with prog5

--- Prog5/mysort.cpp
+++ Prog5/mysort.cpp
@@ -1,75 +1,56 @@
 //	Trevor Dang
 //	CECS 325-02
 //	Prog 5 - Sorting Contest using Threads
-//	03/21/2023
+//	04/05/2023
 //
 //	I certify that this program is my own original work. I did not copy any part of this program 
 //	from any other source. I further certify that I typed each and every line of code in this program.
 
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <thread>
+#include <mutex>
 using namespace std;
 
-int partition(int arr[], int left, int right)
+int partition(int arr[], int left, int right) 
 {
-    int pivot = arr[left];
+    int pivot = arr[right];
+    int l = left - 1;
 
-    int count = 0;
-    for (int i = left + 1; i < right; i++)
+    for (int i = left; i <= right - 1; i++) 
     {
-        if (arr[i] <= pivot)
+        if (arr[i] < pivot) 
         {
-            count++;
+            l++;
+            swap(arr[l], arr[i]);
         }
     }
-    int pivotIdx = left + count;
-    swap(arr[pivotIdx], arr[left]);
-    
-    int i = left, j = right;
-    while (i < pivotIdx && j > pivotIdx)
-    {
-        while (arr[i] <= pivot)
-        {
-            i++;
-        }
-        while (arr[j] > pivot)
-        {
-            j--;
-            
-        }
-        if (i < pivotIdx && j > pivotIdx)
-        {
-            swap(arr[i++], arr[j--]);
-        }
-    }
-    return pivotIdx;
+    swap(arr[l + 1], arr[right]);
+    return (l + 1);
 }
 
-//quick sort function 
-void quickSort(int arr[], int left, int right)
+void quickSort(int arr[], int left, int right) 
 {
-    if (left >= right)
+    if (left < right) 
     {
-        return;
+        int p = partition(arr, left, right);
+
+        quickSort(arr, left, p - 1);
+
+        quickSort(arr, p + 1, right);
     }
-
-    int p = partition(arr, left, right);
-
-    quickSort(arr, left, p - 1);
-
-    quickSort(arr, p + 1, right);
-
 }
 
-void merge(int *arr, int left, int mid, int right)
+void merge(int* arr, int left, int mid, int right)
 {
     int i = left;
     int j = mid;
     int k = 0;
-    int *temp = new int[right - left + 1];
+    int l = right - left + 1;
+    int *temp = new int[l];
     
-    while (i < mid && j < right)
+    while (i < mid && j <= right)
     {
         if (arr[i] < arr[j])
         {
@@ -86,107 +67,77 @@ void merge(int *arr, int left, int mid, int right)
         temp[k++] = arr[i++];
     }
 
-    while (j < right)
+    while (j <= right)
     {
         temp[k++] = arr[j++];
     }
 
-    for (int x = left, y = 0; x < right; x++, y++)
+    for (int i = 0; i < l; ++i)
     {
-        arr[x] = temp[y];
+        arr[left + i] = temp[i];
     }
     delete [] temp;
 }
 
 
-int main(int argc, char* argv[])
+
+int main(int argc, char* argv[]) 
 {
-    if (argc < 3)
-    {
-        cerr << "Usage: " << argv[0] << " Input Output" << endl;
-        return 1;
-    }
-
-
-    ifstream ifile(argv[1]); //input file
-    if (!ifile.is_open())
-    {
-        cerr << "Cannot open input file" << argv[1] << endl;
-    }
-
-    int numbers[1000000]; //array in file that can hold 1 million numbers
-    int count = 0; //counter to keep track of numbers input file
-    int nums;
+    ifstream infile(argv[1]);
     
+    const int max_num = 1000000;
+    const int num_threads = 8;
+    int numbers[max_num];
+    int count = 0;
+    int num;
 
-    while (ifile >> nums && count < 1000000) //while loop that makes sure number of inputs not more than 1 million
+    while (infile >> num) 
     {
-        numbers[count] = nums;
-        count++;
-        if (count > 1000000)
-        {
-            cerr << "Input file contains too many numbers" << endl;
-            return 1;
+        numbers[count++] = num;
+    }
+    infile.close();
+
+    int subSize = count / num_threads;
+    thread threads[num_threads];
+
+    for (int i = 0; i < num_threads; i++) 
+    {
+        int left = i * subSize;
+        int right = (i + 1)*(subSize) - 1;
+
+        if (i == num_threads - 1) {
+            right = count - 1;
         }
-    }
-    ifile.close();
-    
 
-
-    int sizeArr = count;
-    int size = sizeArr/8;
-    
-
-    thread thread1(quickSort, numbers, 0, size - 1);
-    thread thread2(quickSort, numbers, size, 2 * size - 1);
-    thread thread3(quickSort, numbers, 2 * size, 3 * size - 1);
-    thread thread4(quickSort, numbers, 3 * size, 4 * size - 1);
-    thread thread5(quickSort, numbers, 4 * size, 5 * size - 1);
-    thread thread6(quickSort, numbers, 5 * size, 6 * size - 1);
-    thread thread7(quickSort, numbers, 6 * size, 7 * size - 1);
-    thread thread8(quickSort, numbers, 7 * size, sizeArr - 1);
-
-    thread1.join();
-    thread2.join();
-    thread3.join();
-    thread4.join();
-    thread5.join();
-    thread6.join();
-    thread7.join();
-    thread8.join();
-
-
-    int subSize = size;
-
-    for (int i = 0; i < 8; i+=2)
-    {
-        merge(numbers, (i)*(subSize), (i+1)*(subSize), (i+2)*(subSize));
+        threads[i] = thread(quickSort, numbers, left, right);
     }
 
-    for (int j = 0; j < 4; j+=2)
+    for (int i = 0; i < num_threads; i++) 
     {
-        merge(numbers, (j)*(2)*(subSize), (j+1)*(2)*(subSize), (j+2)*(2)*(subSize));
+        threads[i].join();
     }
 
-    merge(numbers, 0, (4)*(subSize), (8)*(subSize));
-
-
-    /*ofstream ofile(argv[2]); //output file that holds sorted array
-    if (!ofile.is_open())
+    int merge_num = subSize;
+    while (merge_num < count) 
     {
-        cerr << "Cannot open output file" << argv[2] << endl;
-    }*/
-
-    for (int i = 0; i < sizeArr; i++)
-    {
-        //ofile << numbers[i] << endl;
-        cout << numbers[i] << endl;
+        for (int i = 0; i < count; i += (2)*(merge_num)) 
+        {
+            int left = i;
+            int mid = i + merge_num;
+            int right = min(i + (2)*(merge_num) - 1, count - 1);
+            
+            merge(numbers, left, mid, right);
+        }
+        merge_num *= 2;
     }
-    //ofile << numbers[sizeArr - 1];*/
-    
-    
+
+    for (int i = 0; i < count; i++) 
+    {
+        cout << numbers[i] << "\n";
+    }
+
     return 0;
-
 }
+
 
 

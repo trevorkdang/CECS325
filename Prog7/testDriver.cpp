e6ee5be31f19a06304c3d72636f4553360ec9d4f
TKD10 <103336180+TKD10@users.noreply.github.com>
2023-05-05 03:29:21 -0700

    finished with prog7

--- Prog7/testDriver.cpp
+++ Prog7/testDriver.cpp
@@ -1,7 +1,7 @@
 //	Trevor Dang
 //	CECS 325-02
-//	Prog 6 - 3N + 1
-//	04/18/2023
+//	Prog 7 - 3N + 1 (BigInt)
+//	05/04/2023
 //
 //	I certify that this program is my own original work. I did not copy any part of this program 
 //	from any other source. I further certify that I typed each and every line of code in this program.
@@ -17,163 +17,203 @@ class BigInt
 {
     private:
         vector<char> v;
+
     public:
         BigInt()
         {
             v.push_back(0);
         }
-
         BigInt(int n)
         {
-            char digit;
+            char dig;
             while (n > 9)
             {
-                digit = n % 10;
+                dig = n % 10;
                 n /= 10;
-                v.push_back(digit);
+                v.push_back(dig);
             }
-            digit = n;
-            v.push_back(digit);
+            dig = n;
+            v.push_back(dig);
         }
-
-        BigInt(string str)
+        BigInt(string s)
         {
-            for (int i = str.size() - 1; i >= 0; i--)
+            for (char c : s)
             {
-                v.push_back(str[i]);
+                if (isdigit(c))
+                {
+                    v.push_back(c);  
+                }
+                    
             }
+            if (v.empty())
+            {
+                v.push_back(0);
+            }
+                
         }
 
-        BigInt operator+(BigInt b)
+
+        BigInt operator+(BigInt a) 
         {
             BigInt res;
-            int car = 0;
-            int i = 0, j = 0;
-            while (i < v.size() || j < b.v.size() || car > 0)
-            {
-                int sum = car;
-                if (i < v.size())
-                {
-                    sum += v[i];
-                }
-                if (j < b.v.size())
-                {
-                    sum += b.v[j];
-                }
+            char car = 0;
+            char dig = 0;
+            int i = 0;
+            res.v.clear();
 
-                res.v.push_back(sum % 10);
-                car = sum / 10;
+            while (i < min(v.size(), a.v.size()))
+            {
+                car += v[i] + a.v[i];
+                dig = car % 10;
+                res.v.push_back(dig);
+                car = car / 10; 
                 i++;
-                j++;
+            }
+            while (i < v.size())
+            {
+                car += v[i];
+                dig = car % 10;
+                res.v.push_back(dig);
+                car = car / 10;
+                i++;
+            }
+            while (i < a.v.size())
+            {
+                car += a.v[i];
+                dig = car % 10;
+                res.v.push_back(dig);
+                car = car / 10;
+                i++;
+            }
+            if (car > 0)
+            {
+                res.v.push_back(car);
             }
             return res;
         }
 
-        BigInt operator++()
+        BigInt operator++() 
         {
             *this = *this + BigInt(1);
             return *this;
         }
-
         BigInt operator++(int)
         {
-            BigInt temp(*this);
+            BigInt temp = *this;
             *this = *this + BigInt(1);
             return temp;
         }
 
-        BigInt operator*(BigInt b)
+        BigInt operator*(BigInt a)
         {
             BigInt res;
-            int car = 0;
+            int car;
+            res.v.resize(v.size() + a.v.size());
+
             for (int i = 0; i < v.size(); i++)
             {
                 car = 0;
-                BigInt temp;
-                for (int j = 0; j < i; j++)
+                for (int j = 0; j < a.v.size(); j++)
                 {
-                    temp.v.push_back(0);
+                    car += res.v[i + j] + v[i] * a.v[j];
+                    res.v[i + j] = car % 10;
+                    car = car / 10;
                 }
-                for (int j = 0; j < b.v.size(); j++)
-                {
-                    int ans = v[i] * b.v[j] + car;
-                    temp.v.push_back(ans % 10);
-                    car = ans / 10;
-                }
-                if (car > 0)
-                {
-                    temp.v.push_back(car);
-                }
-                res = res + temp;
+                res.v[i + a.v.size()] = car;
+            }
+
+            while (res.v.size() > 1 && res.v.back() == 0)
+            {
+                res.v.pop_back();
+            }
+
+            return res;
+        }
+
+
+        BigInt half() 
+        {
+            BigInt res = *this;
+            int num;
+            int car = 0;
+
+            for (int i = res.v.size() - 1; i >= 0; i--)
+            {
+                num = res.v[i] + (10 * car);
+                res.v[i] = num / 2;
+                car = num % 2;
+            }
+            while (res.v.back() == 0)
+            {
+                res.v.pop_back();
             }
             return res;
         }
 
-        BigInt half() const
+        bool operator<(BigInt a) 
         {
-            BigInt res; // this is just the Bigint num
-            int car = 0; // made a carry value
-            for (int i = v.size() - 1; i >= 0; i--) //just a loop that goes through each digit in the BigINt
+            if (v.size() < a.v.size())
             {
-                int cur = (v[i] + car * 10) /2;
-                if (i != 0 || cur != 0)
-                {
-                    res.v.push_back(cur);
-                }
-                car = v[i] % 2;
+            return true; 
             }
-            return res;
+                
+            if (v.size() > a.v.size())
+            {
+                return false;
+            }
+                
+            for (int i = v.size() - 1; i >= 0; i--)
+            {
+                if (v[i] < a.v[i])
+                {
+                    return true;
+                }
+                    
+                else if (v[i] > a.v[i])
+                {
+                    return false;
+                }
+            }
+
+            return false; 
         }
-        bool isOdd() const
+        bool isOdd() 
         {
             return v[0] % 2 != 0;
         }
-        bool isEven() const
+        bool isEven() 
         {
             return v[0] % 2 == 0;
         }
 
-        bool operator==(BigInt b) const
-        {
-            for (int i = 0; i < v.size(); i++)
-            {
-                if (v[i] != b.v[i])
-                {
-                    return false;
-                }
-            }
-            return true;
-        }
 
-        bool operator<(BigInt b) const
+        bool operator==(BigInt a) 
         {
-            if (v.size() > b.v.size())
-            {
+            if (v.size() != a.v.size())
                 return false;
-            }
-            for (int i = v.size() - 1; i >= 0; i--)
-            {
-                if (v[i] < b.v[i])
-                {
-                    return true;
-                }
-                if (v[i] > b.v[i])
-                {
-                    return false;
-                }
-            }
-            return false;
+            return v == a.v;
         }
 
-    friend ostream& operator<<(ostream& os, const BigInt& num)
+    friend ostream& operator<<(ostream& os, const BigInt& n) 
     {
-        for (int i = 0; i < num.v.size(); i++)
+        if (n.v.size() <= 8)
         {
-            cout << (int)num.v[i];
+            for (int i = n.v.size() - 1; i >= 0; i--)
+            {
+                os << +n.v[i];
+            }
+        }
+        else
+        {
+            os << +n.v[n.v.size() - 1] << ".";
+            for (int i = n.v.size() - 2; i >= n.v.size() - 8; i--)
+            {
+                os << +n.v[i];
+            }
+            os << 'e' << n.v.size() - 1;
         }
         return os;
     }
-
 };
 
 
@@ -225,7 +265,7 @@ void findThreeNp1(BigInt n, ThreeNp1& Np1, bool printSteps = false)
         Np1.odd++;
         Np1.steps++;
         BigInt tempN(n);
-        findThreeNp1(n*BigInt(3)+BigInt(1), Np1, printSteps);
+        findThreeNp1(tempN*BigInt(3)+BigInt(1), Np1, printSteps);
     }
 
     else
@@ -238,9 +278,9 @@ void findThreeNp1(BigInt n, ThreeNp1& Np1, bool printSteps = false)
 int main()
 {
     BigInt MAX(INT_MAX);
-    cout << "The Largest integer is " << MAX << endl;
+    cout << "The largest integer is " << MAX << endl;
     cout << "Twice the largest integer is " << MAX + MAX << endl;
-    BigInt start(12);
+    BigInt start(INT_MAX);
     bool printSteps = false;
     ThreeNp1 N = {start, 0, 0, 0, 0};
     findThreeNp1(start, N, printSteps);
